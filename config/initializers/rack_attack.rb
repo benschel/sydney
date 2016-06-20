98d707b3cb91129144146dcdb0b9cb488d520890
Ben Henschel <benschel170@gmail.com>
2016-06-20 14:03:51 -0400

    Revert "whitelist ip"

--- config/initializers/rack_attack.rb
+++
@@ -1,6 +0,0 @@
-class Rack::Attack
-  Rack::Attack.whitelist('allow from localhost') do |req|
-    # Requests are allowed if the return value is truthy
-    '69.64.203.18' == req.ip || '::1' == req.ip
-  end
-end
