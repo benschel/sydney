f4821dddf2c8d80a308e5bbabc69d4f2708ee848
Ben <benschel170@gmail.com>
2016-07-23 21:04:06 -0400

    Added nav

--- app/assets/javascripts/display.js
+++ app/assets/javascripts/display.js
@@ -1,48 +1,50 @@
 // Display js
-$('#display-container').ready(function() {
-    var $users = $('.user');
-    var i = 0;
-    var numUsers = $users.length;
+$(document).on('page:load page:change', function() {
+    if ($('#display-container').length) {
+        var $users = $('.user');
+        var i = 0;
+        var numUsers = $users.length;
 
-    var triggerWinner = function(currentUser) {
-        if ($(currentUser).find('.stars').length) {
-            $('.star-icon').addClass('animate-star');
-        }
-    };
-
-    var userLoop = function() {
-        var $currentUser = $users.eq(i);
-        
-        $currentUser.removeClass('fade-out-left').addClass('fade-in-right');
-
-        setTimeout(function() {
-            var $icons = $currentUser.find('.user-mention');
-            var j = 0;
-
-            var animateIcons = function() {
-                $icons.eq(j).addClass('animate-icon');
-
-                j++;
-
-                if (j === $icons.length) {
-                    clearInterval(animateIconInterval);
-                    triggerWinner($currentUser);
-                }
-            };
-            animateIcons();
-            var animateIconInterval = setInterval(animateIcons, 250);
-        }, 800);
-
-        setTimeout(function() {
-            $currentUser.removeClass('fade-in-right').addClass('fade-out-left');
-            i++;
-
-            if (i === numUsers) {
-                location.reload();
+        var triggerWinner = function(currentUser) {
+            if ($(currentUser).find('.stars').length) {
+                $('.star-icon').addClass('animate-star');
             }
-        }, 299000);
-    };
+        };
 
-    userLoop();
-    setInterval(userLoop, 300000);
+        var userLoop = function() {
+            var $currentUser = $users.eq(i);
+            
+            $currentUser.removeClass('fade-out-left').addClass('fade-in-right');
+
+            setTimeout(function() {
+                var $icons = $currentUser.find('.user-mention');
+                var j = 0;
+
+                var animateIcons = function() {
+                    $icons.eq(j).addClass('animate-icon');
+
+                    j++;
+
+                    if (j === $icons.length) {
+                        clearInterval(animateIconInterval);
+                        triggerWinner($currentUser);
+                    }
+                };
+                animateIcons();
+                var animateIconInterval = setInterval(animateIcons, 250);
+            }, 800);
+
+            setTimeout(function() {
+                $currentUser.removeClass('fade-in-right').addClass('fade-out-left');
+                i++;
+
+                if (i === numUsers) {
+                    location.reload();
+                }
+            }, 299000);
+        };
+
+        userLoop();
+        setInterval(userLoop, 300000);
+    }
 });
--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,43 +1,45 @@
 $(document).on('page:load page:change', function() {
-    $('.tip').tipr({
-        'mode': 'top'
-    });
-
-    var triggerWinner = function(el) {
-        var $user = $(el).parent('.user');
-
-        if ($user.find('.stars').length) {
-            $('.star-icon').addClass('animate-star')
-        }
-    };
-
-    $.each($('#mentions-container .user-mentions-container'), function(k, v) {
-        var counter = 0;
-        var $icons = $(v).children();
-
-        if ($icons.length) {
-            var interval = setInterval(function() {
-                $icons.eq(counter).addClass('animate-icon');
-
-                if (counter === $icons.length) {
-                    clearInterval(interval);
-                    triggerWinner(v);
-                }
-
-                counter++;
-            }, 250);
-        }
-    });
-
-    setInterval(function() {
-        var $firstUser = $('.current-user-group .user:first-child');
-        $firstUser.animate({'opacity': 0}).slideUp(500, function() {
-            var $parentContainer = $(this).parent();
-            $('.users-group:not(.current-user-group)').addClass('current-user-group');
-            $parentContainer.removeClass('current-user-group');
-
-            var $removedUser = $(this).detach();
-            $removedUser.show().css({'opacity': 1}).appendTo($parentContainer);
+    if ($('#mentions-container').length) {
+        $('.tip').tipr({
+            'mode': 'top'
         });
-    }, 60000);
+
+        var triggerWinner = function(el) {
+            var $user = $(el).parent('.user');
+
+            if ($user.find('.stars').length) {
+                $('.star-icon').addClass('animate-star')
+            }
+        };
+
+        $.each($('#mentions-container .user-mentions-container'), function(k, v) {
+            var counter = 0;
+            var $icons = $(v).children();
+
+            if ($icons.length) {
+                var interval = setInterval(function() {
+                    $icons.eq(counter).addClass('animate-icon');
+
+                    if (counter === $icons.length) {
+                        clearInterval(interval);
+                        triggerWinner(v);
+                    }
+
+                    counter++;
+                }, 250);
+            }
+        });
+
+        setInterval(function() {
+            var $firstUser = $('.current-user-group .user:first-child');
+            $firstUser.animate({'opacity': 0}).slideUp(500, function() {
+                var $parentContainer = $(this).parent();
+                $('.users-group:not(.current-user-group)').addClass('current-user-group');
+                $parentContainer.removeClass('current-user-group');
+
+                var $removedUser = $(this).detach();
+                $removedUser.show().css({'opacity': 1}).appendTo($parentContainer);
+            });
+        }, 60000);
+    }
 });
