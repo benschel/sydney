783419ed4be8494b2744a9a15a5902c75d397d93
Ben <benschel170@gmail.com>
2016-07-24 11:37:53 -0400

    Code cleanup

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,45 +1,73 @@
-$(document).on('page:load page:change', function() {
-    if ($('#mentions-container').length) {
-        $('.tip').tipr({
-            'mode': 'top'
-        });
+// Main js
+var MentionsApp = (function() {
+    var app = {
+        init: function() {
+            var self = this;
 
-        var triggerWinner = function(el) {
-            var $user = $(el).parent('.user');
+            this.initTipr();
+            this.showMentions();
 
-            if ($user.find('.stars').length) {
-                $('.star-icon').addClass('animate-star')
-            }
-        };
+            setInterval(function() {
+                self.slideThroughUsers();
+            }, 60000);
+        },
 
-        $.each($('#mentions-container .user-mentions-container'), function(k, v) {
-            var counter = 0;
-            var $icons = $(v).children();
+        initTipr: function() {
+            $('.tip').tipr({
+                'mode': 'top'
+            });
+        },
 
-            if ($icons.length) {
-                var interval = setInterval(function() {
-                    $icons.eq(counter).addClass('animate-icon');
+        showMentions: function() {
+            var self = this;
 
-                    if (counter === $icons.length) {
-                        clearInterval(interval);
-                        triggerWinner(v);
-                    }
+            $.each($('.user-mentions-container'), function(k, v) {
+                var counter = 0;
+                var $icons = $(v).children();
 
-                    counter++;
-                }, 250);
-            }
-        });
+                if ($icons.length) {
+                    var iconInterval = setInterval(function() {
+                        $icons.eq(counter).addClass('animate-icon');
 
-        setInterval(function() {
+                        if (counter === $icons.length) {
+                            clearInterval(iconInterval);
+                            self._triggerWinner(v);
+                        }
+
+                        counter++;
+                    }, 250);
+                }
+            });
+        },
+
+        slideThroughUsers: function() {
             var $firstUser = $('.current-user-group .user:first-child');
+
             $firstUser.animate({'opacity': 0}).slideUp(500, function() {
                 var $parentContainer = $(this).parent();
-                $('.users-group:not(.current-user-group)').addClass('current-user-group');
+
+                $('.users-group:not(.current-user-group').addClass('current-user-group');
                 $parentContainer.removeClass('current-user-group');
 
                 var $removedUser = $(this).detach();
                 $removedUser.show().css({'opacity': 1}).appendTo($parentContainer);
             });
-        }, 60000);
+        },
+
+        _triggerWinner: function(el) {
+            var $user = $(el).parent('.user');
+
+            if ($user.find('.stars').length) {
+                $('.star-icon').addClass('animate-star');
+            }
+        }
+    };
+
+    return app;
+})();
+
+$(document).on('page:load page:change', function() {
+    if ($('#mentions-container').length) {
+        MentionsApp.init();
     }
 });
